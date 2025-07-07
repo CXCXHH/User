f8592593a6c79ddbc915f6dbdb804479ba073f9d
CXCXHH <m15111642499@163.com>
2025-07-07 21:45:10 +0800

    PI速度环上位机调节

--- main.c
+++ main.c
@@ -7,9 +7,15 @@
 #include "Encoder.h"    
 #include "motor_ctrl.h"  
 #include "gw_gray.h"
-#include "Menu.h"    
+#include "Menu.h"
+#include "PID.h"  
+#include "Control.h"  
 
-float Target_Speed = 10.0f; // 目标速度
+PID_TypeDef pid_motor1; 
+PID_TypeDef pid_motor2; 
+
+//期望速度
+float Target_Speed = 0.0f;
 
 int main(void)
 {
@@ -18,6 +24,8 @@ int main(void)
 	BEEP_OFF();
 	Menu_Init();
 	NVIC_EnableIRQ_Init();
+	PID_Init(&pid_motor1, 40.5f, 2.05f, 0.0f);
+    PID_Init(&pid_motor2, 40.0f, 2.05f, 0.0f);
 	while(1)
 	{
 		Menu_loop();
@@ -27,6 +35,7 @@ int main(void)
 void TIMER_0_INST_IRQHandler(void)
 {
 	static uint16_t tick_count_10ms = 0;// 10ms任务计数器
+	static uint16_t tick_count_20ms = 0;// 20ms任务计数器
 	static uint16_t tick_count_100ms = 0;// 10ms任务计数器
 	static uint16_t tick_count_1000ms = 0;// 1000ms任务计数器
 
@@ -36,12 +45,27 @@ void TIMER_0_INST_IRQHandler(void)
 		tick_count_10ms = 0;
 		Key_Read(); // 读取按键状态
 		Encoder_Speed(); // 计算编码器速度
+		int pwm_output1 = (int)PID_realize(&pid_motor1, Target_Speed, Motor1_Speed);
+        int pwm_output2 = (int)PID_realize(&pid_motor2, Target_Speed, Motor2_Speed);
+
+        Set_Motor1_Speed(pwm_output1);
+        Set_Motor2_Speed(pwm_output2);
+
 		vofa_sendData(Target_Speed, Motor1_Speed, Motor2_Speed);
 	}
 
+	// 20ms任务
+	if (++tick_count_20ms >= 100)
+	{
+
+		tick_count_20ms = 0;		
+	}
+
+	
 	// 100ms任务
 	if (++tick_count_100ms >= 100)
 	{
+
 		tick_count_100ms = 0;		
 	}
 
